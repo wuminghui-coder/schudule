74504fdcd5ef74ea084110ca5a97c946acd78253
wuminghui <1334878991@qq.com>
2025-07-28 14:57:39 +0800

    加入过滤日志功能

--- src/util/log.h
+++ src/util/log.h
@@ -3,13 +3,25 @@
 #include <stdio.h>
 #include "timestamp.h"
 
+enum {
+    LOG_LEVEL_ALL,
+    LOG_LEVEL_DEBUG,
+    LOG_LEVEL_INFO,
+    LOG_LEVEL_WAR,
+    LOG_LEVEL_ERR,
+};
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL  LOG_LEVEL_ERR
+#endif
+
 #define RED_COLOR "\033[0;31m"
 #define RED_COLOR1 "\033[0;32m"
 #define RED_COLOR2 "\033[0;33m"
 #define RED_COLOR3 "\033[0;37m"
 #define GREEN_COLOR "\033[1;32m"   
 
-#define LOG_LEVEL(color, fmt, ...) \
+#define LOG_LEVEL_PRINTF(color, fmt, ...) \
     do { \
         char date_buffer[100]; \
         get_now_ms_date(date_buffer, sizeof(date_buffer)); \
@@ -17,10 +29,10 @@
                color, date_buffer, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
     } while (0)
 
-#define ERR(fmt, ...) LOG_LEVEL(RED_COLOR, fmt, ##__VA_ARGS__)
-#define WAR(fmt, ...) LOG_LEVEL(RED_COLOR1, fmt, ##__VA_ARGS__)
-#define LOG(fmt, ...) LOG_LEVEL(RED_COLOR2, fmt, ##__VA_ARGS__)
-#define DBG(fmt, ...) LOG_LEVEL(GREEN_COLOR, fmt, ##__VA_ARGS__)
+#define ERR(fmt, ...) if (LOG_LEVEL <=  LOG_LEVEL_ERR) LOG_LEVEL_PRINTF(RED_COLOR, fmt, ##__VA_ARGS__)
+#define WAR(fmt, ...) if (LOG_LEVEL <=  LOG_LEVEL_WAR) LOG_LEVEL_PRINTF(RED_COLOR1, fmt, ##__VA_ARGS__)
+#define LOG(fmt, ...) if (LOG_LEVEL <=  LOG_LEVEL_INFO) LOG_LEVEL_PRINTF(RED_COLOR2, fmt, ##__VA_ARGS__)
+#define DBG(fmt, ...) if (LOG_LEVEL <=  LOG_LEVEL_DEBUG) LOG_LEVEL_PRINTF(GREEN_COLOR, fmt, ##__VA_ARGS__)
 
 static inline void printfChar(uint8_t *data, int len)
 {
